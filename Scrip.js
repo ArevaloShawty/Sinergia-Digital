0f35b6e4eb922dbfc75951ae9ba4e6f9949a6b58
ArevaloShawty <arevalochanico9@gmail.com>
2024-10-27 12:38:17 -0600

    Scrip.js

--- Scrip.js
+++ Scrip.js
@@ -13,3 +13,11 @@ document.getElementById("contactForm").addEventListener("submit", function(event
     this.reset();
 });
 
+function toggleMenu() {
+    const menuItems = document.querySelector('.R'); // Cambié a 'R'
+    if (menuItems.style.display === 'block') {
+        menuItems.style.display = 'none'; // Oculta el menú
+    } else {
+        menuItems.style.display = 'block'; // Muestra el menú
+    }
+}
